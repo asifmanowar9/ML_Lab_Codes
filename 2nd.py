d554ba490e6478f2965e272d71973da54c5da583
asifmanowar9 <raiyanasif939@gmail.com>
2025-08-01 22:56:05 +0600

    2nd

---
+++ 2nd.py
@@ -0,0 +1,14 @@
+str1="R.P. Shaha "
+str2="University."
+
+print(str1+str2)
+
+length=len(str1+str2)
+print(length)
+
+num = int(input("Enter a number:"))
+
+if num % 2 == 0:
+    print("The number " f"{num} is even.")
+else:
+    print("The number " f"{num} is odd.")
